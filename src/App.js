65b9e6d625c29d5d2c9dced886a9f56e801eb8ff
syifakitstudentpens <57141184+syifakitstudentpens@users.noreply.github.com>
2022-06-21 08:06:52 +0700

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import { Register } from './Components/Register'
+import { Login } from './Components/Login'
+import { auth, db } from './Config/Config'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+
+  state = {
+      user: null,
+  }
+
+  componentDidMount() {
+
+      // getting user info for navigation bar
+      auth.onAuthStateChanged(user => {
+          if (user) {
+              db.collection('SignedUpUsersData').doc(user.uid).get().then(snapshot => {
+                  this.setState({
+                      user: snapshot.data().Name
+                  })
+              })
+          }
+          else {
+              this.setState({
+                  user: null
+              })
+          }
+      })
+
+  }
+
+  render() {
+      return (
+          <ProductsContextProvider>
+              <CartContextProvider>
+                  <BrowserRouter>
+                      <Switch>
+                          {/* home */}
+                          <Route exact path='/' component={() => <Home user={this.state.user} />} />
+                          {/* register */}
+                          <Route path="/register" component={Register} />
+                          {/* login */}
+                          <Route path="/login" component={Login} />
+                          <Route component={NotFound} />
+                      </Switch>
+                  </BrowserRouter>
+              </CartContextProvider>
+          </ProductsContextProvider>
+      )
+  }
 }
 
-export default App;
+export default App
